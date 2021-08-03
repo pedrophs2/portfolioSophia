44956cf2e527b817945f3e871bb9f590d20b359f
Aiandra  Meira <dinha.aiandra@yahoo.com.br>
2021-08-03 20:50:30 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+<h1 align="center">Portfólio Sophia</h1>
+
+<p align="center">
+    <img alt="Preview" src="assets/img/bannerOne.png">
+    
+</p>
+
+<p align="center">
+    <img alt="Preview" src="assets/img/bannerTwo.png">
+</p>
+
+## 💻 Projeto
+
+Portfolio Sophia é uma website elegante e bem desenvolvido com o objetivo de apresentar algo simples mas ao mesmo tempo bem estruturado.
+
+## ✨ Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+-   [HTML]
+-   [CSS]
+-   [Javascript]
+
+---
+
+Feito com ❤ por Aiandra Meira 👋🏻 &nbsp;[Portfólio Pessoal](https://aiandralves.com.br)
---
+++ assets/js/scripts.js
@@ -0,0 +1,218 @@
+/**---------- Menu Show -----------**/
+
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');
+
+// Menu Show
+
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu');
+    });
+}
+
+// Menu Hidden
+
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+    });
+}
+
+/**---------- Remove Menu Mobile ------------**/
+
+const navLink = document.querySelectorAll('.nav-link');
+
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.remove('show-menu');
+}
+navLink.forEach((n) => n.addEventListener('click', linkAction));
+
+/**------------ Accordion Skills ------------**/
+
+const skillsContent = document.getElementsByClassName('skills-content');
+const skillsHeader = document.querySelectorAll('.skills-header');
+
+function toggleSkills() {
+    let itemClass = this.parentNode.className;
+
+    for (i = 0; i < skillsContent.length; i++) {
+        skillsContent[i].className = 'skills-content skills-close';
+    }
+
+    if (itemClass === 'skills-content skills-close') {
+        this.parentNode.className = 'skills-content skills-open';
+    }
+}
+
+skillsHeader.forEach((el) => {
+    el.addEventListener('click', toggleSkills);
+});
+
+/**---------- Qualification Tabs ------------**/
+
+const tabs = document.querySelectorAll('[data-target]');
+const tabContents = document.querySelectorAll('[data-content]');
+
+tabs.forEach((tab) => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target);
+
+        tabContents.forEach((tabContent) => {
+            tabContent.classList.remove('qualification-active');
+        });
+
+        target.classList.add('qualification-active');
+
+        tab.forEach((tab) => {
+            tab.classList.add('qualification-active');
+        });
+
+        tab.classList.add('qualification-active');
+    });
+});
+
+/**------------ Services Modal --------------**/
+
+const modalViews = document.querySelectorAll('.services-modal');
+const modalBtns = document.querySelectorAll('.services-btn');
+const modalCloses = document.querySelectorAll('.services-modal-close');
+
+let modal = function (modalClick) {
+    modalViews[modalClick].classList.add('active-modal');
+};
+
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click', () => {
+        modal(i);
+    });
+});
+
+modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener('click', () => {
+        modalViews.forEach((modalView) => {
+            modalView.classList.remove('active-modal');
+        });
+    });
+});
+
+/**------------ Portfolio Swipper --------------**/
+
+let swiper = new Swiper('.portfolio-container', {
+    cssMode: true,
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
+
+/**------------ Testimonial --------------**/
+
+let swiperTestimonial = new Swiper('.testimonial-container', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 48,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+    },
+    breakpoints: {
+        568: {
+            slidesPerView: 2,
+        },
+    },
+});
+
+/**------------ Scroll Sections Active --------------**/
+
+const sections = document.querySelectorAll('section[id]');
+
+function scrollActive() {
+    const scrollY = window.pageYOffset;
+
+    sections.forEach((current) => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id');
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document
+                .querySelector('.nav-menu a[href*=' + sectionId + ']')
+                .classList.add('active-link');
+        } else {
+            document
+                .querySelector('.nav-menu a[href*=' + sectionId + ']')
+                .classList.remove('active-link');
+        }
+    });
+}
+
+window.addEventListener('scroll', scrollActive);
+
+/**------------ Change Background --------------**/
+
+function scrollHeader() {
+    const nav = document.getElementById('header');
+
+    if (this.scrollY >= 80) {
+        nav.classList.add('scroll-header');
+    } else {
+        nav.classList.remove('scroll-header');
+    }
+}
+
+window.addEventListener('scroll', scrollHeader);
+
+/**------------ Show Scroll Up --------------**/
+
+function scrollUp() {
+    const scrollUp = document.getElementById('scroll-up');
+
+    if (this.scrollY >= 560) {
+        scrollUp.classList.add('show-scroll');
+    } else {
+        scrollUp.classList.remove('show-scroll');
+    }
+}
+
+window.addEventListener('scroll', scrollUp);
+
+/**------------ Dark Light Theme --------------**/
+
+const themeBtn = document.getElementById('theme-btn');
+const darkTheme = 'dark-theme';
+const iconTheme = 'uil-sun';
+
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+const getCurrentTheme = () =>
+    document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () =>
+    themeBtn.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun';
+
+if (selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](
+        darkTheme
+    );
+    themeBtn.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](
+        iconTheme
+    );
+}
+
+// Activate / Deactivate the theme manually with the button
+themeBtn.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme);
+    themeBtn.classList.toggle(iconTheme);
+
+    localStorage.setItem('selected-theme', getCurrentTheme());
+    localStorage.setItem('selected-icon', getCurrentIcon());
+});
